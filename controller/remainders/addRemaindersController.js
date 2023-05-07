d554ec75151179018ebd0c4cf49bba36318c3fd0
Vetali1985 <100028292+Vetali1985@users.noreply.github.com>
2023-05-07 11:21:00 +0300

    Create addRemaindersController.js

---
+++ controller/remainders/addRemaindersController.js
@@ -0,0 +1,6 @@
+const { BadRequest } = require('http-errors');
+
+const addRemaindersController = async (req, res, next) => {
+  res.status(201).console.log('kjnbjh');
+};
+module.exports = { addRemaindersController };
